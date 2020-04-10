8cf8b78972749e52bda83d901e17a3a8eddef5cb
Leeya Davis <leeya463@gmail.com>
2020-04-09 20:17:21 -0400

    finish more easy1

---
+++ easy1_rectangles_and_squares.rb
@@ -0,0 +1,29 @@
+# Given the following class:
+
+class Rectangle
+  def initialize(height, width)
+    @height = height
+    @width = width
+  end
+
+  def area
+    @height * @width
+  end
+end
+
+# Write a class called Square that inherits from Rectangle, and is used like this:
+# class Square < Rectangle
+#   def initialize(side)
+#     @height = side # hint, hint, this code
+#     @width = side # isn't DRY
+#   end
+# end
+
+class Square < Rectangle
+  def initialize(side)
+    super(side, side)
+  end
+end
+
+square = Square.new(5)
+puts "area of square = #{square.area}"
