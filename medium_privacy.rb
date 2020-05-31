33d61c97168f878e1fae45897f0946eda1e92665
Leeya Davis <leeya463@gmail.com>
2020-05-31 08:26:53 -0400

    finish medium privacy FE

--- medium_privacy.rb
+++ medium_privacy.rb
@@ -1,15 +1,19 @@
 class Machine
   def start
-    self.flip_switch(:on)
+    flip_switch(:on)
   end
 
   def stop
-    self.flip_switch(:off)
+    flip_switch(:off)
+  end
+
+  def switch_status
+    puts "The switch is currently #{switch}."
   end
 
   private
 
-  attr_writer :switch
+  attr_accessor :switch
 
   def flip_switch(desired_state)
     self.switch = desired_state
@@ -17,3 +21,9 @@ class Machine
 end
 
 # Modify this class so both flip_switch and the setter method switch= are private methods.
+
+new_machine = Machine.new
+new_machine.start
+new_machine.switch_status
+new_machine.stop
+new_machine.switch_status
