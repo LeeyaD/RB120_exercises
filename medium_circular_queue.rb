8ce145664c355430001d622786a8f0956c4b0919
Leeya Davis <leeya463@gmail.com>
2020-06-01 20:49:31 -0400

    finish medium circular queue

--- medium_circular_queue.rb
+++ medium_circular_queue.rb
@@ -1,24 +1,24 @@
 class CircularQueue
-  #obj instantiated w/ 1 attribute, buffer_size
-  # and 2 methods:
-  #enqueue - to add an obj to the queue
-  #dequeue - to remove (and return) the oldest obj from the queue. Should return nil if the queue is empty
-
-  # You may assume that none of the values stored in the queue are nil (however, nil may be used to designate empty spots in the buffer).
-
   def initialize(buffer_size)
     @buffer_size = buffer_size
     @array = Array.new
   end
 
   def enqueue(value)
-
+    array.shift if array.size == buffer_size
+    array << value
   end
 
   def dequeue
-
+    return nil if array.empty?
+    array.shift
   end
+
+  private
+
+  attr_accessor :buffer_size, :array
 end
+
 queue = CircularQueue.new(3)
 puts queue.dequeue == nil
 
